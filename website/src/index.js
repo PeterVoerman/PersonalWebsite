8eba98dde072ff1f3e2990cbc6bab25e60e3918a
Peter <peter.jpv@hotmail.com>
2022-01-05 00:39:21 +0100

    Code splitting

--- website/src/index.js
+++ website/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { Suspense, lazy} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { createTheme, ThemeProvider } from '@mui/material';
-import {
-  NavigationBar,
-  Home,
-  WhatsAppAnalyzer,
-  SnakeField,
-  FractalTree,
-  SolarSystem
-} from './components'
+
+const NavigationBar = lazy(() => import('./components/NavigationBar/NavigationBar'))
+const Home = lazy(() => import('./components/home/home'))
+const WhatsAppAnalyzer = lazy(() => import('./components/whatsappAnalyzer/WhatsAppAnalyzer'))
+const SnakeField = lazy(() => import('./components/Snake/SnakeField'))
+const FractalTree = lazy(() => import('./components/FractalTree/FractalTree'))
+const SolarSystem = lazy(() => import('./components/SolarSystem/SolarSystem'))
 
 const theme = createTheme({
   palette: {
@@ -21,16 +20,18 @@ const theme = createTheme({
 
 ReactDOM.render(
     <Router>
+      <Suspense fallback={<div>Loading...</div>}>
       <ThemeProvider theme={theme}>
       <NavigationBar/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Home/>}/>
         <Route path='/snake' element={<SnakeField/>}/>
         <Route path='/fractaltree' element={<FractalTree/>}/>
         <Route path='/solarsystem' element={<SolarSystem/>}/>
         <Route path="/whatsappanalyzer" element={<WhatsAppAnalyzer/>}/>
       </Routes>
       </ThemeProvider>
+      </Suspense>
     </Router>,
 
   document.getElementById('root')
